4da1cfc9ada5a1ac578797ede75ab67d95182c95
pavankalyankota <102729054+pavankalyankota@users.noreply.github.com>
2023-04-20 11:27:57 +0100

    Add files via upload

---
+++ 2.Printing of a string & number value.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my repository");
+            Console.WriteLine(12345);
+            Console.ReadLine();
+        }
+    }
+}
