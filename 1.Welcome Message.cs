9ebff8373d34bf378ec109c6d335fa03541e6d73
pavankalyankota <102729054+pavankalyankota@users.noreply.github.com>
2023-04-20 11:27:37 +0100

    Add files via upload

---
+++ 1.Welcome Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WelcomeMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my C#.Net repository");
+            Console.WriteLine("Thanks for your time and patience");
+            Console.ReadLine();
+        }
+    }
+}
