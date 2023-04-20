d70ce260e9cfc20aaeb2996c319e6aa5841c849f
pavankalyankota <102729054+pavankalyankota@users.noreply.github.com>
2023-04-20 12:47:39 +0100

    Add files via upload

---
+++ 3.Printing of values with the help of variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p = "Pavan";
+            int a = 99;
+            double b = 99.99;
+            Console.WriteLine(p);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 4.Printing of name & age with help of variables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Pavan";
+            int age = 25;
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.ReadLine();
+        }
+    }
+}
