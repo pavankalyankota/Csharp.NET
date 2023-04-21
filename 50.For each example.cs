9aa8968b21b8e8a3cb3db44ead39e68206a57de0
pavankalyankota <102729054+pavankalyankota@users.noreply.github.com>
2023-04-21 11:02:50 +0100

    Add files via upload

---
+++ 50.For each example.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp89
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 20, 30, 40, 50, 60, 70, 80 };
+            foreach (int a in array)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
